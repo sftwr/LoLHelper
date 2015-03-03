4db818218cc8c6c20c9b231a38858ba9336944bc
sauronkitty <evilmaniac@gmail.com>
2015-03-03 02:10:51 -0600

    Updated default population of itemslist

--- app/src/main/java/se/lolhelper/ManagerItems.java
+++ app/src/main/java/se/lolhelper/ManagerItems.java
@@ -22,10 +22,10 @@ public class ManagerItems {
 
         // This is where we load all Item data into the DataLists
 
-        pItems.addItem(1, "Syndra", "Born with immense magical potential, Syndra loves nothing more than exercising the incredible power at her command. With each passing day, her mastery of magical force grows more potent and devastating. Refusing any notion of balance or restraint, Syndra wants only to retain control of her power, even if it means annihilating the authorities that seek to stop her.", null);
-        pItems.addItem(2, "Elise", "Elise's entrancing beauty and grace conceal the pitiless, black heart of a deadly predator. With ruthless cunning, she lures the unsuspecting with promises of favor from the spider god. Having exchanged her humanity to become something far more sinister, Elise sacrifices the innocent to maintain her power and seemingly eternal youth. No one can fathom how many have been caught in her web, slain to feed her insatiable hunger.", null);
-        pItems.addItem(3, "Ahri", "Unlike other foxes that roamed the woods of southern Ionia, Ahri had always felt a strange connection to the magical world around her; a connection that was somehow incomplete. Deep inside, she felt the skin she had been born into was an ill fit for her and dreamt of one day becoming human. Her goal seemed forever out of reach, until she happened upon the wake of a human battle.", null);
-        pItems.addItem(4, "Amumu", "Amumu is a diminutive, animated cadaver who wanders the world, trying to discover his true identity. He rose from an ancient Shuriman tomb bound in corpse wrappings with no knowledge of his past, consumed with an uncontrollable sadness.", null);
+        pItems.addItem(1, "item1", "item1 desc", null);
+        pItems.addItem(2, "item2", "item2 desc", null);
+        pItems.addItem(3, "item3", "item3 desc", null);
+        pItems.addItem(4, "item4", "item4 desc", null);
         return;
     }
 
